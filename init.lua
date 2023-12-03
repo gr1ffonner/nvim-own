f4dbf1da5933d21974b32f30690d316b67c3943e
gr1ffonner <danya.rodnyakov228@gmail.com>
2023-12-03 20:18:02 +0300

    project tree pt.1

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- Base
 require("config.options")
 require("config.mappings")
 require("config.lazy")
+
+-- Plug Walk
+require("plugins.plugins")
+require("mason").setup()
--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<Leader>i", ":write<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Leader>]", "gt")
 vim.keymap.set("n", "<Leader>[", "gT")
+vim.keymap.set('n', '<leader>e', ':Neotree left reveal<CR>')
+vim.keymap.set('n', '<leader>o', ':Neotree close<CR>')
