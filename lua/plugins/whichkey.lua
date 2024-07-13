8194f23303b0368e0565217f590c9debd4c8eae2
griffonner <danya.rodnyakov228@gmail.com>
2024-07-13 19:07:52 +0300

    saving buffer refactoring

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,9 +1,15 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-    end,
-    opts = {},
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 0
+	end,
+	opts = {
+		layout = {
+			align = 3,
+			width = { max = 25 },
+			spacing = 3,
+		},
+	},
 }
